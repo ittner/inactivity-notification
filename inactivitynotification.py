4a2751d3348c7c6f62ad11e699f5489f7c4a2c35
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2010-09-03 14:33:02 -0300

    Add documentation and a saner encoding handler

--- inactivitynotification.py
+++ inactivitynotification.py
@@ -1,6 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+USAGE = u"""
+This program monitor files which are expected to be updated at periodic
+intervals and emits desktop notifications when they become outdated.
+
+The following commands are accecpted:
+
+  start
+    Starts the server process. The program will not return until finished.
+    If there is another server process running for the current user, just
+    prints an error and exits.
+  
+  stop
+    Stops any running instance.
+
+  timer <minutes>
+    Sets the verification/notification interval to the given number of
+    minutes.
+
+  add <file> <timeout> <short message> [<long message> [icon]]
+    Adds a file to the verification queue.
+  
+  remove <file> [...]
+    Remove the given file from the verification queue.
+    
+  list
+    List the currently monitored files.
+
+
+"""
+
+MONITOR_BUS = "br.com.ittner.FileMonitor"
+MONITOR_OBJECT = "/br/com/ittner/FileMonitor"
+MONITOR_INTERFACE = "br.com.ittner.FileMonitor"
+
+
 import sys
 import os
 import os.path
@@ -9,11 +44,8 @@ import dbus.glib
 import gobject
 import time
 import datetime
-
-MONITOR_BUS = "br.com.ittner.FileMonitor"
-MONITOR_OBJECT = "/br/com/ittner/FileMonitor"
-MONITOR_INTERFACE = "br.com.ittner.FileMonitor"
-
+import locale
+import codecs
 
 class MonitoredFile(object):
 
@@ -88,9 +120,11 @@ class Monitor(dbus.service.Object):
 
     @dbus.service.method(MONITOR_INTERFACE, in_signature="i")
     def set_timer(self, seconds):
+        if seconds < 1: return False    # Error
         if self.timer:
             gobject.source_remove(self.timer)
         self.timer = gobject.timeout_add_seconds(seconds, self._process)
+        return True
 
     def _process(self):
         for f in self.files:
@@ -108,16 +142,9 @@ def find_server():
     except: pass
     return None
 
+
 def usage():
-    print("Monitor files for expected updates")
-    print("Commands:")
-    print(" start")
-    print(" stop")
-    print(" timer <seconds>")
-    print(" addstart <file> <timeout> <short message> [long message [icon]]")
-    print(" add <file> <timeout> <short message> [long message [icon]]")
-    print(" remove <file> [...]")
-    print(" list")
+    stderr.write(USAGE)
 
 
 def interpret(args):
@@ -135,7 +162,7 @@ def interpret(args):
         if srv.try_register():
             loop.run()
         else:
-            print("Failed to get ownership of the bus. Already running?")
+            stdout.write("Failed to get ownership of the bus. Already running?\n")
         return
 
     if not command in [ "stop", "add", "remove", "list", "timer" ]:
@@ -144,7 +171,7 @@ def interpret(args):
 
     obj = find_server()
     if not obj:
-        print("Server not found")
+        stdout.write("Server not found\n")
         return
 
     if command == "stop":
@@ -153,17 +180,17 @@ def interpret(args):
 
     if command == "timer":
         if len(args) == 0:
-            print("How many seconds?")
+            stdout.write("How many seconds?\n")
             return
         try:
             obj.set_timer(int(args[0]))
         except:
-            print("Bad number")
+            stdout.write("Bad number\n")
         return
 
     if command == "remove":
         if len(args) == 0:
-            print("No file to remove")
+            stdout.write("No file to remove\n")
             return
         for fname in args:
             fname = os.path.realpath(fname)
@@ -172,7 +199,7 @@ def interpret(args):
 
     if command == "list":
         for mf in obj.list_files():
-            print(mf[0] + '\t' + str(mf[1]) + '\t' + str(mf[2]) + '\t' + str(mf[3]) + '\t' + str(mf[4]))
+            stdout.write(mf[0] + '\t' + str(mf[1]) + '\t' + mf[2] + '\t' + mf[3] + '\t' + mf[4] + "\n")
 
     if command == "add":
         if len(args) < 3 or len(args) > 5:
@@ -182,12 +209,12 @@ def interpret(args):
         fname = args[0]
         fullname = os.path.realpath(fname)
         if not os.path.exists(fullname):
-            print("File not found")
+            stdout.write("File not found\n")
             return
         try:
             timeout = int(args[1])
         except:
-            print("Timeout must be a number of seconds. (sei, deveria ser horas...)")
+            stdout.write("Timeout must be a number of seconds. (sei, deveria ser horas...)\n")
             return
         summary = args[2]
         if len(args) > 3:
@@ -204,4 +231,8 @@ def interpret(args):
 
 
 if __name__ == '__main__':
-    interpret(sys.argv)
+    enc = "utf-8"       # use Unicode or die, baby
+    stdout = codecs.getwriter(enc)(sys.stdout)
+    stderr = codecs.getwriter(enc)(sys.stderr)
+    fixed_argv = [ s.decode(enc) for s in sys.argv ]
+    sys.exit(interpret(fixed_argv) or 0)
